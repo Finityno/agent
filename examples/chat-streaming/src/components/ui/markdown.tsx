d62c7e008cd1a3718846e5cdd2009da461ed44c5
Sondre <68125004+Finityno@users.noreply.github.com>
2025-06-09 02:59:02 +0200

    Removed unused stuff
    remove unused stuff,
    
    need to remove old migrating logic as well for this

--- examples/chat-streaming/src/components/ui/markdown.tsx
+++ examples/chat-streaming/src/components/ui/markdown.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils"
 import { marked } from "marked"
-import { memo, useId, useMemo, useCallback } from "react"
+import { memo, useId, useMemo } from "react"
 import ReactMarkdown, { Components } from "react-markdown"
 import remarkBreaks from "remark-breaks"
 import remarkGfm from "remark-gfm"
